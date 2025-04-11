72451d37c887a19907a2ed24a6af0b701f24369c
Gabriel Brandão Machado <101605450+0LB-i@users.noreply.github.com>
2025-04-11 15:24:06 -0300

    Create install-unifi-server.sh

---
+++ install-unifi-server.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+# Versão padrão do UniFi Server
+default_version="9.0.108"
+
+# Perguntar a versão do UniFi Server, com valor padrão
+read -p "Enter the version of UniFi Server you want to install [default: ${default_version}]: " unifi_version
+unifi_version=${unifi_version:-$default_version}
+
+# Adicionar repositório do MongoDB
+cat << 'EOF' > /etc/yum.repos.d/mongodb-org-8.0.repo
+[mongodb-org-8.0]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/8.0/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://pgp.mongodb.com/server-8.0.asc
+EOF
+
+# Atualizar e instalar dependências
+yum -y update
+yum install epel-release -y
+yum install mongodb-org java-17-openjdk-devel unzip wget -y
+
+# Criar usuário ubnt
+useradd ubnt
+
+# Iniciar e habilitar o serviço MongoDB
+systemctl start mongod.service
+systemctl enable mongod.service
+systemctl status mongod.service
+
+# Baixar o UniFi Server na versão escolhida
+cd /opt
+wget "https://dl.ui.com/unifi/${unifi_version}/UniFi.unix.zip"
+
+# Descompactar o UniFi Server
+unzip -qo /opt/UniFi.unix.zip -d /opt
+
+# Ajustar permissões
+chown -R ubnt:ubnt /opt/UniFi
+
+# Criar arquivo de serviço systemd
+cat << 'EOF' > /etc/systemd/system/unifi.service
+# Systemd unit file for UniFi Controller
+[Unit]
+Description=UniFi AP Web Controller
+After=syslog.target network.target
+
+[Service]
+Type=simple
+User=ubnt
+WorkingDirectory=/opt/UniFi
+# CONF PARA ALMA 9
+ExecStart=/usr/lib/jvm/jre-17/bin/java --add-opens=java.base/java.time=ALL-UNNAMED $JAVA_OPTS -jar /opt/UniFi/lib/ace.jar start
+# ExecStart=/usr/bin/java -Xmx1024M -jar /opt/UniFi/lib/ace.jar start
+ExecStop=/usr/bin/java -jar /opt/UniFi/lib/ace.jar stop
+SuccessExitStatus=143
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Habilitar e iniciar o serviço UniFi
+systemctl daemon-reexec
+systemctl daemon-reload
+systemctl enable unifi.service
+systemctl start unifi.service
+systemctl status unifi.service
+
+# Limpar o arquivo zip
+rm -rf /opt/UniFi.unix.zip
+
+# Reboot para garantir que o sistema esteja pronto
+read -p "Installation is complete. Would you like to restart your system now? (y/n): " reboot_choice
+if [[ "$reboot_choice" =~ ^[Yy]$ ]]; then
+  reboot
+else
+  echo "Installation complete. You can manually restart the system later."
+fi
